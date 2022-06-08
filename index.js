e4bd3c0f8a3bafce6415a26523ad1116bc82c8f4
Larg913 <leonramirez1998@gmail.com>
2022-06-08 19:39:45 -0400

    completed

--- index.js
+++ index.js
@@ -15,4 +15,11 @@ function playGame() {
 
 while (playAgain === true) {
     playGame()
+    let userChoice = window.prompt('Would you like to play the game again? Answer yes or no.')
+    if(userChoice === 'yes'){
+            playAgain = true
+        } else {
+            playAgain = false
+            window.alert('Thanks for playing the game!')
+        }
 }
