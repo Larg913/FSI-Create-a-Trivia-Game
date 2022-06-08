0861be25c68c4d4ec2ba1c0666c533a57491374d
Larg913 <leonramirez1998@gmail.com>
2022-06-08 19:38:24 -0400

    user answers to variables

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let userName = window.prompt('Please enter your name:')
 
 for(let i = 0; i < questions.length; i++){
-    console.log(questions[i])
+    let question = questions[i]
+    let userAnswer = window.prompt(question.text)
 }
