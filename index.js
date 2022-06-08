5dd91ba25c951cbd8b852b234bc8ec987b4d7bc8
Larg913 <leonramirez1998@gmail.com>
2022-06-08 19:39:24 -0400

    Want to play again

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 let userName = window.prompt('Please enter your name:')
 let userScore = 0
+let playAgain = true
 
-for(let i = 0; i < questions.length; i++){
-    let question = questions[i]
-    let userAnswer = window.prompt(question.text)
-    if(userAnswer === question.correctAnswer){
-        userScore = userScore + 10
+function playGame() {
+    for(let i = 0; i < questions.length; i++){
+        let question = questions[i]
+        let userAnswer = window.prompt(question.text)
+        if(userAnswer === question.correctAnswer){
+            userScore = userScore + 10
+        }
     }
+    window.alert('Your score is: '+userScore)
 }
 
-window.alert('Your score is: '+userScore)
+while (playAgain === true) {
+    playGame()
+}
